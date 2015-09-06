bbd9252a5512a482f703572486d9c1b12d173e54
Filip Defar <filip.defar@gmail.com>
2015-09-06 20:58:41 +0200

    Enabled writing to file

--- cvml.rb
+++ cvml.rb
@@ -1,3 +1,5 @@
+require 'erb'
+
 class CVML
   def initialize
     @data = { personal_data: {} }
@@ -17,19 +19,23 @@ class CVML
 
   personal_attributes :name, :location, :homepage, :email
 
-  def print
-    puts @data
+  def write_to_file(filename)
+    renderer = ERB.new(File.read("template.html.erb"))
+    generated_html = renderer.result(binding)
+    File.open(filename, 'w') do |file|
+      file.write(generated_html)
+    end
   end
 end
 
 def CV(&block)
   instance = CVML.new
   instance.instance_eval &block
+  instance.write_to_file("result_cv.html")
 end
 
 
 CV do
   name "Filip Defar"
   email "filip.defar@gmail.com"
-  print
 end
